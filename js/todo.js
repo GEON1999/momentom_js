989e60a8f8dbd9dcd6283696c7651bf20f8d3a9d
GEON1999 <101832525+GEON1999@users.noreply.github.com>
2022-04-12 16:41:52 +0900

    toDoList

---
+++ js/todo.js
@@ -0,0 +1,21 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoList = document.querySelector("#todo-list");
+
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newToDo;
+    toDoList.appendChild(li);
+}
+
+function handelToDoSubmit(event) {
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
+    paintToDo(newToDo);
+}
+
+toDoForm.addEventListener("submit", handelToDoSubmit);
+
