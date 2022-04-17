7afd69eee3d05734a324a887c97bf852123c0fbf
GEON1999 <101832525+GEON1999@users.noreply.github.com>
2022-04-17 13:30:45 +0900

    bg.full

--- js/weather.js
+++ js/weather.js
@@ -8,10 +8,11 @@ function onGeoOk(position) {
     fetch(url)
     .then(responese => responese.json())
     .then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const temp = document.querySelector("#weather span:last-child");
+        const weather = document.querySelector("#weather span:last-child");
+        const temp = document.querySelector("#weather span:first-child");
+        temp.innerText = `${data.main.temp}°`    
         weather.innerText = data.weather[0].main ;
-        temp.innerText = `${data.main.temp}°` 
+      
     });
 
 }
